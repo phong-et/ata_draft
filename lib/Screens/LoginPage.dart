b259cebf5e556a3f62ae1f5ba007833dc027b739
Phillip <phillip7.et@gmail.com>
2020-02-09 23:17:10 +0700

    fix bottom padding on keyboard show

--- lib/Screens/LoginPage.dart
+++ lib/Screens/LoginPage.dart
@@ -3,30 +3,9 @@ import 'package:ata/Widgets/Login/LoginButton.dart';
 import 'package:ata/Widgets/Login/PasswordField.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class LoginPage extends StatelessWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
   final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // void _incrementCounter() {
-  //   setState(() {
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,8 +13,8 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Attedance Tracking App'),
       ),
       body: Center(
-        child: Container(
-          color: Colors.white,
+        child: SingleChildScrollView(
+          //color: Colors.white,
           child: Padding(
             padding: const EdgeInsets.all(36.0),
             child: Column(
@@ -65,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
+      //resizeToAvoidBottomPadding: false 
     );
   }
 }
