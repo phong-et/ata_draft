0417b9e1cbb9a8bd3d804662e78dbabafcc14159
Phillip <phillip7.et@gmail.com>
2020-02-09 23:19:16 +0700

    create HomePage after login
    has three tabs, includes icon and above text

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,20 +1,64 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Route"),
+        title: const Text('BottomNavigationBar Sample'),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-            Navigator.pop(context);
-          },
-          child: Text('Log Out!'),
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Business'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            title: Text('School'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
--- lib/Widgets/ATA.dart
+++ lib/Widgets/ATA.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ATA {
-  static const TextStyle TEXT_STYLE = TextStyle(fontFamily: 'Montserrat', fontSize: 25.0);
-  static const EdgeInsets CONTENT_PADDING = EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0);
-  static OutlineInputBorder outLineInputBorder = OutlineInputBorder(borderRadius: BorderRadius.circular(30.0));
+  static const TextStyle TEXT_STYLE = TextStyle(fontFamily: 'Montserrat', fontSize: 15.0);
+  static const EdgeInsets CONTENT_PADDING = EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0);
+  static OutlineInputBorder outLineInputBorder = OutlineInputBorder(borderRadius: BorderRadius.circular(15.0));
 }
