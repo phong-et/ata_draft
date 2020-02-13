039e9140765e56d06cfd4599cd5f70b94b0dfbea
Phillip <phillip7.et@gmail.com>
2020-02-13 16:58:14 +0700

    make request login to firebase by http
    Note : 
    QueryString is same body 
    URI is lib support parse json query string to url of post request

--- lib/Models/UserModel.dart
+++ lib/Models/UserModel.dart
@@ -1,25 +1,71 @@
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as JSON;
+import 'dart:async';
+
 enum Status { Uninitialized, Authenticated, Authenticating, Unauthenticated }
 
 class UserModel with ChangeNotifier {
   String id;
-  String username;
-  String password;
+  String _username;
+  static const String FIREBASE_SIGNIN_URL =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword';
+  static const String FIREBASE_API_KEY =
+      'AIzaSyAP-e1B-iwRYn82wlooyOrXa20pXh8j-nY';
+  String get username => _username;
+  set username(String username) {
+    _username = username;
+    notifyListeners();
+  }
+
+  String _password;
+  String get password => _password;
+  set password(String password) {
+    _password = password;
+    notifyListeners();
+  }
+
   String loginToken;
   String team;
 
   Status _status = Status.Uninitialized;
   Status get status => _status;
-  Future<bool> signIn(String email, String password) async {
+  void printJson(jsonData) {
+    JSON.JsonEncoder encoder = JSON.JsonEncoder.withIndent('  ');
+    String prettyPrint = encoder.convert(jsonData);
+    print(prettyPrint);
+  }
+
+  Future<dynamic> signIn(String email, String password) async {
     try {
       _status = Status.Authenticating;
       // login firebase Auth REST API
       // ...
+      var body = {
+        'key': FIREBASE_API_KEY,
+        'email': 'phillip7.et@gmail.com',
+        'password': '12345678',
+      };
+      var uri = Uri.parse(FIREBASE_SIGNIN_URL);
+      uri = uri.replace(queryParameters: body);
+      print('Logining...');
+      final response = await http.post(uri);
+      if (response.statusCode == 200) {
+        // If server returns an OK response, parse the JSON
+        print('Login successfully');
+        printJson(JSON.jsonDecode((response.body)));
+      } else {
+        // If that response was not OK, throw an error.
+        print('Login Failed');
+        printJson(response.statusCode);
+        printJson(response.body);
+      }
       notifyListeners();
       return true;
     } catch (e) {
       _status = Status.Unauthenticated;
       notifyListeners();
+      print(e);
       return false;
     }
   }
@@ -30,6 +76,5 @@ class UserModel with ChangeNotifier {
     _status = Status.Unauthenticated;
     notifyListeners();
     return Future.delayed(Duration.zero);
-
   }
 }
--- lib/Widgets/Login/LoginButton.dart
+++ lib/Widgets/Login/LoginButton.dart
@@ -1,11 +1,14 @@
+import 'package:ata/Models/UserModel.dart';
 import 'package:flutter/material.dart';
 import 'package:ata/Widgets/ATA.dart';
+import 'package:provider/provider.dart';
 
 class LoginButton extends StatelessWidget {
   final String label;
   const LoginButton({this.label = "Login"});
   @override
   Widget build(BuildContext context) {
+    final userModel = Provider.of<UserModel>(context);
     return Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
@@ -13,9 +16,19 @@ class LoginButton extends StatelessWidget {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: ATA.CONTENT_PADDING,
-        onPressed: () {
+        onPressed: () async {
           //Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-          Navigator.pushReplacementNamed(context, '/home');
+          //Navigator.pushReplacementNamed(context, '/home');
+          bool status = await userModel.signIn(userModel.username, userModel.password);
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(title: Text("Dialog"), content:Text('''
+                    username = ${userModel.username}
+                    password = ${userModel.password}
+                    login status : $status
+                    '''));
+              });
         },
         child: Text(label,
             textAlign: TextAlign.center,
